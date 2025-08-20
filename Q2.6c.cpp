b9458a1c63e15e862333cf7e0008960a8f6d48e4
hunarkaur07 <hkaur5_be24@thapar.edu>
2025-08-21 02:32:00 +0530

    Initial commit to new repo

--- Q2.6c.cpp
+++ Q2.6c.cpp
@@ -2,19 +2,32 @@
 using namespace std;
 
 int main() {
+    //matrix 1
     int m1, n1, t1, m2, n2, t2;
     cout << "Enter rows, columns, non-zero elements of first matrix: ";
     cin >> m1 >> n1 >> t1;
     int A[t1+1][3];
-    A[0][0]=m1; A[0][1]=n1; A[0][2]=t1;
-    for(int i=1;i<=t1;i++) cin >> A[i][0] >> A[i][1] >> A[i][2];
+    A[0][0]=m1; //no of rows
+    A[0][1]=n1; //cols
+    A[0][2]=t1; //non zero elements
+    for(int i=1;i<=t1;i++) {
+        cin >> A[i][0] >> A[i][1] >> A[i][2];
+    }
 
+    //matrix 2
     cout << "Enter rows, columns, non-zero elements of second matrix: ";
     cin >> m2 >> n2 >> t2;
-    if(n1 != m2){ cout<<"Multiplication not possible\n"; return 0; }
+    if(n1 != m2){ 
+        cout<<"Multiplication not possible\n"; 
+        return 0;
+    }
     int B[t2+1][3];
-    B[0][0]=m2; B[0][1]=n2; B[0][2]=t2;
-    for(int i=1;i<=t2;i++) cin >> B[i][0] >> B[i][1] >> B[i][2];
+    B[0][0]=m2; 
+    B[0][1]=n2; 
+    B[0][2]=t2;
+    for(int i=1;i<=t2;i++) {
+        cin >> B[i][0] >> B[i][1] >> B[i][2];
+    }
 
 
     int C[1000][3]; // max non-zero elements
